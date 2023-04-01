6db6486ce1810f008f9dff03c5e510e727651fc7
TimiAndrei <timiandrei223@gmail.com>
2023-04-01 23:34:56 +0300

    updated frontend

--- Webapp/Product/views.py
+++ Webapp/Product/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse
 from .forms import CreateUserForm, addTaskForm
 import cx_Oracle
 import pandas as pd
+from user import user
+from tasks import task
+from user_list import userList
 
 
 cx_Oracle.init_oracle_client(lib_dir=r"D:/Oracle_libraries/instantclient_21_8")
@@ -34,21 +37,6 @@ def register(request):
     context = {'form':form}
     return render(request,'register.html',context=context) 
 
-def addTasks(*args):
-    conn=connection()
-    cursor = conn.cursor()
-    sqlTxt = 'INSERT INTO Tasks values('
-    for item in args:
-        sqlTxt += str(item) + ", "
-    sqlTxt += ")"
-
-    cursor.execute(sqlTxt)
-
-    records = cursor.fetchall()
-
-    df = pd.DataFrame.from_records(records, columns = [x[0] for x in cur.description])
-    return df
-
 def addtask(request):
     if request.method == 'GET':
         return render(request, 'addtask.html', {'task':{}})
@@ -59,10 +47,15 @@ def addtask(request):
             description = form.cleaned_data['description']
             difficulty = form.cleaned_data['difficulty']
             deadline = form.cleaned_data['deadline']
-            duration = form.cleaned_data['duration']
+            visibility = form.cleaned_data['visibility']
             task_type = form.cleaned_data['task_type']
             username = form.cleaned_data['username']
-            addTasks(title, description, difficulty, deadline, duration, task_type, username)
+            new_task = task(title, description, visibility, difficulty, deadline, task_type)
+
+            for person in userList.users:
+                if person.username == username:
+                    person.addTask(new_task)
+            # addTasks(title, description, difficulty, deadline, duration, task_type, username)
     
     return redirect('listtasks')
 
@@ -77,29 +70,3 @@ def listtasks(request):
         tasks.append({"title": row[0], "description": row[1], "difficulty": row[2], "deadline": row[3], "duration": row[4], "task_type": row[5], "username": row[6]})
     conn.close()
     return render(request, 'listtasks.html', {'tasks': tasks})
-
-def addUser(*args):
-    sqlTxt = 'INSERT INTO Users values('
-    for item in args:
-        sqlTxt += str(item) + ", "
-    sqlTxt += ")"
-
-    cur.execute(sqlTxt)
-
-    records = cur.fetchall()
-
-    df = pd.DataFrame.from_records(records, columns = [x[0] for x in cur.description])
-    return df
-
-def getUsers():
-    sqlTxt = 'SELECT * FROM Users'
-
-    cur.execute(sqlTxt)
-
-    records = cur.fetchall()
-
-    df = pd.DataFrame.from_records(records, columns = [x[0] for x in cur.description])
-    return df
-
-
-
